66f69b1755190b571b6bf69d76cbac6514d76a4d
Sinays <pro.yanis01@gmail.com>
2022-11-13 22:13:15 +0100

    Add converter L2C and C2L prototype

--- src/mvc/Model/Network/Antenna/GeoPosition/GeoPosition.cpp
+++ src/mvc/Model/Network/Antenna/GeoPosition/GeoPosition.cpp
@@ -1,4 +1,5 @@
 #include "GeoPosition.hpp"
+#include "math.h"
 
 GeoPosition::GeoPosition(sf::Vector2<float> lambert) : m_lambert{lambert}, m_coords{} {
     L2C();
@@ -27,9 +28,13 @@ void GeoPosition::setCoords(const sf::Vector2<float>& coords) {
 }
         
 void GeoPosition::L2C() {
-
+    float refX = abs(1037842.881126 - 1012539.918657);
+    float refY = abs(6737765.159074 - 6760952.253250);
+    m_coords.x = abs(m_lambert.x - 1037842.881126) * 3828 / refX;
+    m_coords.y = abs(m_lambert.y - 6737765.159074) * 3828 / refY;
 }
         
 void GeoPosition::C2L() {
-    
+    m_lambert.x = 1037842.881126 - (m_coords.x * 1037842.881126 / 3828);
+    m_lambert.y = 6737765.159074 + (m_coords.y * 6737765.159074 / 3828);
 }
