0b67bf459a2d071abd0fc34ce8952d56b425d65f
amika8 <amir.lycee2001@gmail.com>
2022-11-23 15:03:19 +0100

    added animations and enhanced visualy

--- src/mvc/Model/Gui/GuiModel.cpp
+++ src/mvc/Model/Gui/GuiModel.cpp
@@ -8,7 +8,6 @@ GuiModel::GuiModel():
 {
 
     tgui::Theme::setDefault(tgui::Theme::create("/home/amir/Desktop/tgui/TGUI-0.9.5/themes/BabyBlue.txt"));
-    
 
     // Create the background image
     auto picture = tgui::Picture::create("/home/amir/Desktop/Master1/ProjetReseauMobile/Projet-Reseaux-M1-UHA/src/mvc/Model/Gui/MenuBackground.jpg");
@@ -17,38 +16,50 @@ GuiModel::GuiModel():
 
     //Add title(label)
     auto title = tgui::Label::create();
-    title->hideWithEffect(tgui::ShowAnimationType::SlideToBottom, 500);
     title->setText("Mulhouse Networking");
-    title->setPosition(320, 160);
+    title->setPosition(225, 160);
     title->getRenderer()->setFont("/home/amir/Desktop/Master1/ProjetReseauMobile/Projet-Reseaux-M1-UHA/src/mvc/Model/Gui/font.ttf");
     title->getRenderer()->setTextColor(sf::Color(255,255,255));
+    title->getRenderer()->setTextOutlineThickness(3);
+    title->getRenderer()->setTextOutlineColor(sf::Color(80,80,80));
     //title->getRenderer()->setBackgroundColor(sf::Color(187,226,233));
-    title->setTextSize(72);
+    title->setTextSize(92);
+    title->showWithEffect(tgui::ShowAnimationType::SlideFromTop, 500);
+
 
     //Add buttons
     auto buttonStart = tgui::Button::create();
-    buttonStart->hideWithEffect(tgui::ShowAnimationType::SlideToBottom, 500);
     buttonStart->setPosition(490, 400);
     buttonStart->setText("Start");
+    buttonStart->setInheritedFont("/home/amir/Desktop/Master1/ProjetReseauMobile/Projet-Reseaux-M1-UHA/src/mvc/Model/Gui/font.ttf");
+    buttonStart->setTextSize(30);
     buttonStart->setSize(300, 50);
     buttonStart->onPress([&]{ std::cout << " button Start pressed" << std::endl; });
+    buttonStart->showWithEffect(tgui::ShowAnimationType::SlideFromTop, 500);
 
 
     auto buttonSettings = tgui::Button::create();
-    buttonSettings->hideWithEffect(tgui::ShowAnimationType::SlideToBottom, 500);
     buttonSettings->setPosition(490, 470);
     buttonSettings->setText("Settings");
+    buttonSettings->setInheritedFont("/home/amir/Desktop/Master1/ProjetReseauMobile/Projet-Reseaux-M1-UHA/src/mvc/Model/Gui/font.ttf");
+    buttonSettings->setTextSize(30);
     buttonSettings->setSize(300, 50);
     buttonSettings->onPress([&]{ std::cout << " button Settings pressed" << std::endl; });
+    buttonSettings->showWithEffect(tgui::ShowAnimationType::SlideFromTop, 500);
+
+
 
     auto buttonQuit = tgui::Button::create();
-    buttonQuit->hideWithEffect(tgui::ShowAnimationType::SlideToBottom, 500);
     buttonQuit->setPosition(490, 540);
     buttonQuit->setText("Quit");
+    buttonQuit->setInheritedFont("/home/amir/Desktop/Master1/ProjetReseauMobile/Projet-Reseaux-M1-UHA/src/mvc/Model/Gui/font.ttf");
+    buttonQuit->setTextSize(30);
     ///buttonQuit->setCallbackId(3);
     //buttonQuit->bindCallback(tgui::Button::LeftMouseClicked);
     buttonQuit->setSize(300, 50);
     buttonQuit->onPress([&]{ std::cout << " button Quit pressed" << std::endl; });
+    buttonQuit->showWithEffect(tgui::ShowAnimationType::SlideFromTop, 500);
+
 
     m_gui.setOverrideMouseCursor(tgui::Cursor::Type::Hand);
     m_gui.add(picture);
