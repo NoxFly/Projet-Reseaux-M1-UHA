b820e6351b27d8086106c212ff659b43f0571f31
NoxFly <dorian.thivolle@gmail.com>
2023-01-08 17:32:33 +0100

    add map visivility toggle feature. SO to be resolved

--- src/common.hpp
+++ src/common.hpp
@@ -3,4 +3,7 @@
 // comment this line so it won't run performances logs
 #define MEASURE_PERF
 
+// uncomment this line to show the tile's boundings and positions (dev & debug)
+// #define TILE_DEBUG
+
 #endif // DEBUG
